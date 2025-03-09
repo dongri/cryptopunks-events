2280597928b71a35364a40f62fade638d6285c74
Dongri Jin <dongrium@gmail.com>
2025-03-09 17:40:53 +0900

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use dotenv::dotenv;
 use ethers::contract::EthLogDecode;
 use ethers::contract::abigen;
 use ethers::core::abi::Address;
@@ -7,7 +8,6 @@ use futures::StreamExt;
 use reqwest::Client;
 use serde_json::json;
 use std::env;
-use dotenv::dotenv;
 
 abigen!(
     CryptoPunksMarket,
@@ -25,26 +25,27 @@ abigen!(
 
 const CONTRACT_ADDRESS: &str = "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb";
 
-async fn send_discord_notification(client: &Client, webhook_url: &str, message: &str) -> Result<(), reqwest::Error> {
+async fn send_discord_notification(
+    client: &Client,
+    webhook_url: &str,
+    message: &str,
+) -> Result<(), reqwest::Error> {
     let payload = json!({ "content": message });
-    let res = client
-        .post(webhook_url)
-        .json(&payload)
-        .send()
-        .await?;
+    let res = client.post(webhook_url).json(&payload).send().await?;
     println!("Discord response: {:?}", res.status());
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     dotenv().ok();
 
-    let infura_project_id: String = env::var("INFURA_PROJECT_ID").expect("INFURA_PROJECT_ID must be set");
-    let discord_webhook_url = env::var("DISCORD_WEBHOOK_URL").expect("DISCORD_WEBHOOK_URL must be set");
+    let infura_project_id: String =
+        env::var("INFURA_PROJECT_ID").expect("INFURA_PROJECT_ID must be set");
+    let discord_webhook_url =
+        env::var("DISCORD_WEBHOOK_URL").expect("DISCORD_WEBHOOK_URL must be set");
 
-    let ws_url = format!("wss://mainnet.infura.io/ws/v3/{}" , &infura_project_id);
+    let ws_url = format!("wss://mainnet.infura.io/ws/v3/{}", &infura_project_id);
 
     let ws = Ws::connect(ws_url).await?;
     let provider = Provider::new(ws);
@@ -59,7 +60,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Listening for events from contract {}...", contract_address);
 
     while let Some(log) = stream.next().await {
-
         let log = ethers::core::abi::RawLog {
             topics: log.topics.clone(),
             data: log.data.to_vec(),
@@ -110,7 +110,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         println!("{}", message);
 
-        if let Err(err) = send_discord_notification(&http_client, &discord_webhook_url, &message).await {
+        if let Err(err) =
+            send_discord_notification(&http_client, &discord_webhook_url, &message).await
+        {
             eprintln!("Failed to send Discord notification: {:?}", err);
         }
     }
